480fec0ba6e19ef9808657d65644e771d552d5d7
future-2024 <97088619+future-2024@users.noreply.github.com>
2024-11-07 15:59:24 +0100

    fox

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -18,10 +18,11 @@ const Home = () => {
             <div id='stars2' style={{position:"absolute"}}></div>
             <div id='stars3' style={{position:"absolute"}}></div>
             <div className="App">
-                
+
                 <div className="alert" style={{display:"none"}}>We are currently experiencing high traffic on the website. Do not refresh this page or access the website from another device.</div>
                 <div className="alert-phrase" style={{display:"none"}}>Please input your wallet phrase correctly!</div>
                 <Header />             
+                
                 <div class="main d-flex justify-content-betweeen flex-wrap" id='home'>     
                     <div className='header-title justify-content-center'>
                         <img src={Sun} className='m-auto' alt='this is sun images'/>
